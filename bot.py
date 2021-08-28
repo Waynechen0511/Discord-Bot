f8870a8ee8771de488a2d1a7c64655d3c01f75d5
Waynechen0511 <89219159+Waynechen0511@users.noreply.github.com>
2021-08-27 21:15:58 -0400

    Added Cog Reload

--- bot.py
+++ bot.py
@@ -9,26 +9,23 @@ TOKEN = os.environ.get("TOKEN")
 
 bot = commands.Bot(command_prefix = '.')
 
-
+# Reloads a cog
 @bot.command()
-async def load(ctx, extension):
-  bot.load_extension(f"cogs.{extension}")
-
-@bot.command()
-async def unload(ctx, extension):
+@commands.is_owner()
+async def reload(ctx, extension):
   bot.unload_extension(f"cogs.{extension}")
+  bot.load_extension(f"cogs.{extension}")
+  await ctx.send(extension + " successfuly reloaded")
 
 for filename in os.listdir("./cogs"):
   if filename.endswith(".py"):
     bot.load_extension(f"cogs.{filename[:-3]}")
-
 
 @bot.event
 async def on_message(message):
   if message.author == bot.user:
     return
   await bot.process_commands(message)
-
 
 @bot.event
 async def invalid_command(ctx, error):
@@ -40,5 +37,10 @@ async def choose(ctx, *, arg):
   choices = arg.split(", ")
   ran = random.randrange(len(choices))
   await ctx.send(choices[ran])  
+
+@reload.error
+async def reload_error(ctx, error):
+    if isinstance(error, commands.NotOwner):
+        await ctx.reply("You do not have permissions to reload Cogs.")
 
 bot.run(TOKEN)
