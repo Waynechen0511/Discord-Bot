f6dcfa1ef450edcc2bf1ce91b25c7bf91d3e8fa7
Wayne Chen <waynechen0511@gmail.com>
2026-08-24 14:03:13 -0400

    fixed image search

---
+++ cogs/Image_Cog.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from ddgs import DDGS
+
+
+class ImageSearch(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        print("Image Search Cogs is now online")
+
+    @commands.command(aliases=["img"])
+    async def image(self, ctx, *, query):
+        results = DDGS().images(query, max_results=1)
+
+        try:
+            result = results[0]
+        except StopIteration:
+            await ctx.send("No images found.")
+            return
+
+        image_url = result["image"]
+
+        embed = discord.Embed()
+        embed.set_image(url=image_url)
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(ImageSearch(bot))
--- cogs/Moderation_Cog.py
+++ cogs/Moderation_Cog.py
@@ -1,12 +1,12 @@
 import discord
 from discord.ext import commands
 
+
 class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         print("Moderation Cogs is now online")
-        
 
     @commands.command()
     async def ping(self, ctx):
@@ -15,21 +15,34 @@ class Moderation(commands.Cog):
     # Clears messages
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx, arg, member : discord.Member = None):
+    async def clear(self, ctx, arg, member: discord.Member = None):
         if member is None:
-            deleted = await ctx.channel.purge(limit = int(arg))
+            deleted = await ctx.channel.purge(limit=int(arg))
             await ctx.send("Deleted {} message(s)".format(len(deleted)))
         else:
-            # Only deletes the messages sent by member in the past arg messages, instead of deleting arg # of messages by member
-            deleted = await ctx.channel.purge(limit = int(arg), check = lambda e: e.author == member)
-            await ctx.send("Deleted {} message(s) from".format(len(deleted)) + " the target member in the previous " + arg + " messages.")
+            deleted = await ctx.channel.purge(
+                limit=int(arg), check=lambda e: e.author == member
+            )
+            await ctx.send(
+                "Deleted {} message(s) from the target member in the previous {} messages.".format(
+                    len(deleted), arg
+                )
+            )
 
     @clear.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("You do not have permissions to manage messages.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply("Please specify an amount of messages to delete")
+
+    # Creates a poll
+    @commands.command()
+    async def poll(self, ctx, *, question):
+        message = await ctx.send(f"📊 **Poll:** {question}")
+        await message.add_reaction("👍")
+        await message.add_reaction("👎")
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
